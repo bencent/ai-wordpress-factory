40e387f72f825122c29cd0fdb1760b78b5ba4f54
Bencent <bencent@gmail.com>
2026-09-14 08:05:28 +0800

    chore: remove generated runtime artifacts from version control

--- debug_validator.py
+++
@@ -1,8 +0,0 @@
-from tools.frontend_validator import FrontendValidator, _HTMLValidationParser
-
-parser = _HTMLValidationParser()
-html = '<div class="hello">Hello</div>'
-parser.set_html(html)
-parser.feed(html)
-print("Errors:", parser.get_errors())
-print("Tag stack:", parser.tag_stack)
